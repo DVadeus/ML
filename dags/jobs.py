c41f75d4f9ad61ab6d1a89900ee5e5797d780f11
DVadeus <jdaviduarte@gmail.com>
2025-09-23 22:32:04 -0500

    fix dags

--- dags/jobs.py
+++ dags/jobs.py
@@ -1,5 +1,5 @@
 from dagster import job
-from .ops import pull_dvc_op, trigger_kaggle_op, collect_result_op, upload_model_to_b2_op
+from dags.ops import pull_dvc_op, trigger_kaggle_op, collect_result_op, upload_model_to_b2_op
 
 @job
 def pipeline():
